6c905a15bc7030da94af695e6918d6dca501f49d
sosahh <1447961434@qq.com>
2018-01-15 16:29:08 +0800

    11

---
+++ BACON/src/main/java/com/jyss/bacon/entity/BaseConfig.java
@@ -0,0 +1,49 @@
+package com.jyss.bacon.entity;
+
+public class BaseConfig {
+	private int id;
+	private String content;
+	private String key;
+	private String title;
+
+	// private int status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/*
+	 * public int getStatus() { return status; }
+	 * 
+	 * public void setStatus(int status) { this.status = status; }
+	 */
+
+}
