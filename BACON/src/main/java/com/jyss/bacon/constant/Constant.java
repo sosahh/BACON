cca3d4b99fdbcb4eaef6f6333293f5ed9c99e5de
sosahh <1447961434@qq.com>
2018-02-02 14:22:17 +0800

    11

--- BACON/src/main/java/com/jyss/bacon/constant/Constant.java
+++ BACON/src/main/java/com/jyss/bacon/constant/Constant.java
@@ -2,7 +2,7 @@ package com.jyss.bacon.constant;
 
 public class Constant {
 	// 图片url拼接
-	public static final String httpUrl = "http://192.168.0.18:8080/";
+	public static final String httpUrl = "http://121.40.29.64:8081/";
 
 
 }
